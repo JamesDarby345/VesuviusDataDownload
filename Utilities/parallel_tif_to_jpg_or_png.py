b187b5301f652ffa13dfc514038472b62344cc25
James Darby <james.darby@mail.mcgill.ca>
2024-03-30 06:37:41 -0400

    parallel conversion is now resumable

--- Utilities/parallel_tif_to_jpg_or_png.py
+++ Utilities/parallel_tif_to_jpg_or_png.py
@@ -33,18 +33,25 @@ Thus you could pass only the root directory as an argument, if you like those de
 python parallel_tif_to_jpg_or_png.py /path/to/directory
 """
 
-def convert_single_tif(file_path, dest_folder, output_format='jpg', quality=85):
+def convert_single_tif(file_path, dest_folder, output_format='jpg', quality=85, overwrite=True):
     try:
+        filename = os.path.basename(file_path)
+        dest_file_path = os.path.join(dest_folder, f"{os.path.splitext(filename)[0]}.{output_format}")
+        
+        # Check if the file exists and if overwrite is False, skip this file
+        if not overwrite and os.path.exists(dest_file_path):
+            return
+        
         img_array = tifffile.imread(file_path)
         if img_array.dtype != 'uint8':
             img_array = (img_array / 256).astype('uint8')
         img = Image.fromarray(img_array)
-        filename = os.path.basename(file_path)
-        dest_file_path = os.path.join(dest_folder, f"{os.path.splitext(filename)[0]}.{output_format}")
+        
         if output_format == 'jpg':
             img.save(dest_file_path, "JPEG", quality=quality)
         elif output_format == 'png':
             img.save(dest_file_path, "PNG")
+        
     except Exception as e:
         print(f"Error processing {file_path}: {e}")
 
@@ -52,17 +59,15 @@ def convert_tif(source_folder, output_format='jpg', quality=85, overwrite=True):
     dest_folder_suffix = '_jpg' if output_format == 'jpg' else '_png'
     dest_folder = f"{source_folder}{dest_folder_suffix}"
     
-    if not overwrite and os.path.exists(dest_folder):
-        print(f"Skipping conversion in {source_folder} as {dest_folder} exists.")
-        return
-    
     if not os.path.exists(dest_folder):
         os.makedirs(dest_folder)
     
-    tif_files = [os.path.join(source_folder, f) for f in os.listdir(source_folder) if f.lower().endswith(('.tif', '.tiff'))]
+    # Exclude files that start with ._ in addition to filtering for .tif and .tiff extensions
+    tif_files = sorted([os.path.join(source_folder, f) for f in os.listdir(source_folder)
+                        if (f.lower().endswith(('.tif', '.tiff')) and not f.startswith('._'))])
 
     with ProcessPoolExecutor() as executor:
-        futures = [executor.submit(convert_single_tif, file_path, dest_folder, output_format, quality) for file_path in tif_files]
+        futures = [executor.submit(convert_single_tif, file_path, dest_folder, output_format, quality, overwrite) for file_path in tif_files]
         for future in futures:
             future.result()
 
@@ -70,13 +75,15 @@ def process_specific_folders(root_directory, target_types, output_format='jpg', 
     for root, dirs, files in os.walk(root_directory):
         if os.path.basename(root) in target_types:
             start_time = time.time()
+            
 
             # Process direct subdirectories of a matching folder
             if os.path.basename(root) != 'layers':
                 for sub_dir in dirs:
-                    specific_folder_path = os.path.join(root, sub_dir)
-                    print(f"Processing .tif files in {specific_folder_path} to {output_format.upper()} with quality {quality}")
-                    convert_tif(specific_folder_path, output_format=output_format, quality=quality, overwrite=overwrite)
+                    if not sub_dir.endswith(('_jpg', '_png')):
+                        specific_folder_path = os.path.join(root, sub_dir)
+                        print(f"Processing .tif files in {specific_folder_path} to {output_format.upper()} with quality {quality}")
+                        convert_tif(specific_folder_path, output_format=output_format, quality=quality, overwrite=overwrite)
             else:
                 convert_tif(root, output_format=output_format, quality=quality, overwrite=overwrite)
             end_time = time.time()
