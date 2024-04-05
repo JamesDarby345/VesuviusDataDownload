6f2d22f293d3b7e955e126846e78c289743910b8
James Darby <james.darby@mail.mcgill.ca>
2024-04-05 12:34:23 -0400

    remove testing break

--- Scroll2/download_volumes_s2.py
+++ Scroll2/download_volumes_s2.py
@@ -87,8 +87,9 @@ def main():
         return
 
     base_url = f"/full-scrolls/{scrollName}.volpkg/volumes/{scanId}/"
-    target_prefix = "./" #change this to point to a different download location
-    target_dir = target_prefix + f"{scrollName}.volpkg/volumes/{scanId}"
+    target_prefix = "/Volumes/16TB_RAID_0/Scroll2" #change this to point to a different download location
+    target_dir = target_prefix + f"/{scrollName}.volpkg/volumes/{scanId}"
+    print(f"Downloading to {target_dir}")
 
     # Number of threads to use for downloading, 
     # ideally enough to saturate the network but not more
@@ -96,11 +97,9 @@ def main():
     threads = 8
 
     # Download the config.json file and set target_dir to be a .volpkg directory for VC compatability
-    subprocess.run(["rclone", "copy", f":http:/full-scrolls/{scrollName}.volpkg/config.json", f"{target_prefix}{scrollName}.volpkg/",
+    subprocess.run(["rclone", "copy", f":http:/full-scrolls/{scrollName}.volpkg/config.json", f"{target_prefix}/{scrollName}.volpkg/",
                     "--http-url", f"http://{username}:{password}@dl.ash2txt.org/", "--progress",
                 f"--multi-thread-streams={threads}", f"--transfers={threads}"], check=True)
-    
-    target_dir = f"./{scrollName}.volpkg/volumes/{scanId}/"
 
     if range_input == "all":
         subprocess.run(["rclone", "copy", f":http:{base_url}", f"{target_dir}",
